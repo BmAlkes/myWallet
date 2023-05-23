af896f8050d2c111b69d271db84eb7415e56bf4b
bmalkes <bmalkes@gmail.com>
2023-05-23 11:59:52 +0300

    feat:add styled-component global

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <>Hello</>;
-}
+import DashBoard from "./pages/Dashboard";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <DashBoard />
+    </>
+  );
+};
 
 export default App;
