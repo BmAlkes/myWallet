eb91d8eed4f36eeff72670a432b37e63fe010b58
bmalkes <bmalkes@gmail.com>
2023-05-27 12:39:02 +0300

    feat:add history card

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,21 +1,36 @@
 import React from "react";
-import { Container, Content } from "./styles";
+import { Container, Content, Filters } from "./styles";
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard/index.tsx";
 
 const List: React.FC = () => {
-  const options = [{ value: "bmalkes", label: "Bmalkes" }];
-  const frutas = [
-    { value: "banana", label: "Banana" },
-    { value: "uva", label: "Uva" },
+  const months = [
+    { value: 6, label: "June" },
+    { value: 7, label: "July" },
+    { value: 8, label: "August" },
+    { value: 9, label: "September" },
+    { value: 10, label: "October" },
+  ];
+  const years = [
+    { value: 2023, label: 2023 },
+    { value: 2024, label: 2024 },
+    { value: 2025, label: 2025 },
   ];
   return (
     <Container>
       <ContentHeader title="Outcome" lineColor="#E44c4e">
-        <SelectInput options={options} />
-        <SelectInput options={frutas} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-reccurent">
+          Recurrent
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventual
+        </button>
+      </Filters>
       <Content>
         <HistoryFinanceCard
           tagColor="#e44c4e"
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -3,3 +3,35 @@ import styled from "styled-components";
 export const Container = styled.div``;
 
 export const Content = styled.div``;
+export const Filters = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+
+  .tag-filter {
+    font-size: 18px;
+    font-weight: 500;
+    background: none;
+    color: ${(props) => props.theme.colors.white};
+    margin: 0 10px;
+    transition: opacity 0.3s;
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+  .tag-filter-reccurent::after {
+    content: "";
+    display: block;
+    margin: 10px auto;
+    width: 55px;
+    border-bottom: 10px solid ${(props) => props.theme.colors.warning};
+  }
+  .tag-filter-eventual::after {
+    content: "";
+    display: block;
+    margin: 10px auto;
+    width: 55px;
+    border-bottom: 10px solid ${(props) => props.theme.colors.success};
+  }
+`;
